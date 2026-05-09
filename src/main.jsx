c519c09b85fda8f4ff98e5bce1849ede5c2e0a0b
Brazza32 <128002183+Brazza32@users.noreply.github.com>
2026-05-09 20:55:46 +1000

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MothersDaySite from "./MothersDaySite";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <MothersDaySite />
+  </React.StrictMode>
+);
